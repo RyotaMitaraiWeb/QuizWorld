b621129dfdba26a73892501ec357016061ca9f25
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-02 00:25:03 +0300

    add AnswerValidationErrorMessages

---
+++ QuizWorld.Common/Constants/ValidationErrorMessages/AnswerValidationErrorMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Common.Constants.ValidationErrorMessages
+{
+    public static class AnswerValidationErrorMessages
+    {
+        public const string IsEmpty = "The answer cannot be empty!";
+        public const string IsTooLong = "The answer must be no longer than 100 characters!";
+    }
+}
