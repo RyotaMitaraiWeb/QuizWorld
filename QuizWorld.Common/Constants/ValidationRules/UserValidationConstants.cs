3b88b1999f2b197d877379d8f8a564fcf807ae4b
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-01 21:56:58 +0300

    restore overrided constants

--- QuizWorld.Common/Constants/ValidationRules/UserValidationConstants.cs
+++ QuizWorld.Common/Constants/ValidationRules/UserValidationConstants.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
 
 namespace QuizWorld.Common.Constants.ValidationRules
 {
-    internal class User
+    public static class UserValidationConstants
     {
+        public static class Username
+        {
+            public const int MinLength = 5;
+            public const int MaxLength = 15;
+            public const string AlphanumericPattern = @"^[a-zA-Z0-9]+$";
+        }
+        public static class Passowrd
+        {
+            public const int MinLength = 6;
+        }
     }
 }
