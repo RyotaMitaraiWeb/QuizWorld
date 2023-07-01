e5d31722e67d1ada0902c8c3362194ee5d3cfed4
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-01 22:00:21 +0300

    add QuizValidationRules

---
+++ QuizWorld.Common/Constants/ValidationRules/QuizValidationRules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Common.Constants.ValidationRules
+{
+    public static class QuizValidationRules
+    {
+        public static class Title
+        {
+            public const int MinLength = 10;
+            public const int MaxLength = 200;
+        }
+
+        public static class Description
+        {
+            public const int MinLength = 10;
+            public const int MaxLength = 500;
+        }
+
+        public static class Questions
+        {
+            public const int MininmumAmount = 1;
+            public const int MaximumAmount = 100;
+        }
+
+    }
+}
