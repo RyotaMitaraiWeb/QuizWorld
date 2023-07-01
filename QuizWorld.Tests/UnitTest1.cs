488e3d62b74e742ffa36c387c0993bb0b057d148
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-01 19:02:54 +0300

    add a sample test

---
+++ QuizWorld.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace QuizWorld.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var a = 2;
+            Assert.Pass();
+        }
+    }
+}
