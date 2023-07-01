a8e6847e996e27177a3d21d1457021d37cd2bd31
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-01 22:13:27 +0300

    add QuestionValidationRules

--- QuizWorld.Common/Constants/ValidationRules/QuestionValidationRules.cs
+++ QuizWorld.Common/Constants/ValidationRules/QuestionValidationRules.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace QuizWorld.Common.Constants.ValidationRules
 {
-    internal class QuestionValidationRules
+    public static class QuestionValidationRules
     {
+        public static class Prompt
+        {
+            public const int MaxLength = 200;
+        }
+
+        public static class SingleChoice
+        {
+            public const int MaximumAmountOfAnswers = 10;
+        }
+
+        public static class MultipleChoice
+        {
+            public const int MaximumAmountOfAnswers = 10;
+        }
+
+        public static class Text
+        {
+            public const int MaximumAmountOfAnswers = 15;
+        }
     }
 }
