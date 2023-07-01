6e150f651203865d2bd60775d41913777602389b
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2023-07-01 21:08:14 +0300

    add Moq (#2)

--- QuizWorld.Tests/UnitTest1.cs
+++ QuizWorld.Tests/UnitTest1.cs
@@ -1,5 +1,19 @@
+using Moq;
+
 namespace QuizWorld.Tests
 {
+    public interface IService
+    {
+        string doSomething();
+    }
+
+    public class Service : IService
+    {
+        public string doSomething()
+        {
+            return "a";
+        }
+    }
     public class Tests
     {
         [SetUp]
@@ -12,6 +26,21 @@ namespace QuizWorld.Tests
         {
             var a = 2;
             Assert.Pass();
+        }
+
+        [Test]
+        public void MockTest()
+        {
+            var mock = new Mock<IService>();
+            
+            mock.Setup(m => m.doSomething())
+                .Returns("b");
+
+            IService service = mock.Object;
+            var test = service.doSomething();
+            Assert.That(test, Is.EqualTo("b"));
+
+            mock.VerifyAll();
         }
     }
 }
