0d25ecc924ea7c706dc87835b67473ba64760f33
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-01 22:24:03 +0300

    add AnswerValidationRules

---
+++ QuizWorld.Common/Constants/ValidationRules/AnswerValidationRules.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Common.Constants.ValidationRules
+{
+    public static class AnswerValidationRules
+    {
+        public const int MaxLength = 100;
+    }
+}
