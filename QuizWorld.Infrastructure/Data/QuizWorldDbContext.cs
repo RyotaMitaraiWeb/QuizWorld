daa680ad0bd35cb0517f19e4acdc4a937e53af2d
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-02 13:56:25 +0300

    configure DB with Identity

---
+++ QuizWorld.Infrastructure/Data/QuizWorldDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Infrastructure.Data
+{
+    public class QuizWorldDbContext : IdentityDbContext
+    {
+        public QuizWorldDbContext(DbContextOptions<QuizWorldDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
