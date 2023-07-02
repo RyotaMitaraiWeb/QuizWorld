1257eb7af68476d494bd532dfa069382bff9d41e
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2023-07-02 15:17:16 +0300

    Change IdentityUser's ID to a GUID (#8)
    * remove empty Common folder
    
    * initialize Infrastructure folder
    
    * install EFCore packages
    
    * add more packages
    
    * add Identity configurations
    
    * configure DB with Identity
    
    * change Identity user's ID to a GUID

---
+++ QuizWorld.Infrastructure/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Infrastructure.Data.Entities
+{
+    /// <summary>
+    /// This entity replaces the standard IdentityUser by using GUID IDs instead of the standard
+    /// string IDs.
+    /// </summary>
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
