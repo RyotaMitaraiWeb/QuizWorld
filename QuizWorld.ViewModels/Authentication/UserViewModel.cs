c61a0bcbea59379832ba384ded282cae677729ff
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-04 01:34:30 +0300

    add some view models

---
+++ QuizWorld.ViewModels/Authentication/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.ViewModels.Authentication
+{
+    /// <summary>
+    /// Represents a user session that will be converted into a JWT.
+    /// </summary>
+    public class UserViewModel
+    {
+        public string Username { get; set; } = null!;
+        public string Id { get; set; } = null!;
+        public string[] Roles { get; set; } = Array.Empty<string>();
+
+    }
+}
