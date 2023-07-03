fdf998e647771d245abf006ff60ebff0ca40065b
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-04 01:31:59 +0300

    seed the database with a few roles

---
+++ QuizWorld.Infrastructure/Data/RoleConfigurer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Infrastructure.Data
+{
+    public class RoleConfigurer : IEntityTypeConfiguration<IdentityRole<Guid>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder)
+        {
+            builder.HasData
+            (
+                new IdentityRole<Guid>()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "User",
+                    NormalizedName = "USER",
+                },
+                new IdentityRole<Guid>()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR",
+                },
+                new IdentityRole<Guid>()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Moderator",
+                    NormalizedName = "MODERATOR",
+                }
+            );
+        }
+    }
+}
