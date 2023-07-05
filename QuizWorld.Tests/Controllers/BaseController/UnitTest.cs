dbc1fd56ca7607106082da97119ab2f46ba86537
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-05 15:22:43 +0300

    add BaseController

---
+++ QuizWorld.Tests/Controllers/BaseController/UnitTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizWorld.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Tests.Controllers.BaseControllerTests
+{
+    public class UnitTest
+    {
+        public BaseController controller { get; set; }
+        public DefaultHttpContext httpContext { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.httpContext = new DefaultHttpContext();
+            this.controller = new BaseController()
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = this.httpContext
+                }
+            };
+        }
+
+        [Test]
+        public void Test_GetJWTReturnsTheStringInAuthorizationHeader()
+        {
+            this.httpContext.Request.Headers["Authorization"] = "a";
+            string jwt = this.controller.GetJWT();
+            Assert.That(jwt, Is.EqualTo("a"));
+        }
+
+        [Test]
+        public void Test_GetJWTReturnsAnEmptyStringIfAuthorizationHeaderIsMissing()
+        {
+            // ensure the header is deleted
+            this.httpContext.Request.Headers.Remove("Authorization");
+
+            string jwt = this.controller.GetJWT();
+            Assert.That(jwt, Is.EqualTo(string.Empty));
+        }
+    }
+}
