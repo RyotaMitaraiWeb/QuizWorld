9d504ee41bcc58e1ca0855dc557b7ffe25bab190
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2023-07-05 15:26:55 +0300

    Add BaseController (#20)
    * add BaseController

---
+++ QuizWorld.Web/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QuizWorld.ViewModels.Authentication;
+using QuizWorld.Web.Contracts;
+
+namespace QuizWorld.Web
+{
+    /// <summary>
+    /// Extends the ControllerBase. Authorizes every route by default. Allows easy access to the user's JWT
+    /// </summary>
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class BaseController : ControllerBase
+    {
+        /// <summary>
+        /// Returns the JWT attached to the Authorization header
+        /// </summary>
+        /// <returns>
+        /// The token or an empty string if the Authorization header is not attached to the request
+        /// </returns>
+        public string GetJWT()
+        {
+            var headers = Request.Headers;
+            if (!headers.ContainsKey("Authorization"))
+            {
+                return string.Empty;
+            }
+
+            headers.TryGetValue("Authorization", out var headerValues);
+            string jwt = headerValues.First();
+            return jwt;
+        }
+    }
+}
