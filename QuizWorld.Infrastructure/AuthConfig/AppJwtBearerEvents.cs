a122f6e09e894c0b04be46152a1fabf844b3ebf2
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2023-07-07 22:54:29 +0300

    Check the JWT blacklist on each authorized request (#24)
    * check JWT blacklist on each authorized request

---
+++ QuizWorld.Infrastructure/AuthConfig/AppJwtBearerEvents.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using QuizWorld.Infrastructure.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Infrastructure.AuthConfig
+{
+    /// <summary>
+    /// Configures the JWT middleware to refer to the JWT blacklist when validating the token
+    /// </summary>
+    public class AppJwtBearerEvents : JwtBearerEvents
+    {
+        private readonly IJwtService jwtService;
+        public AppJwtBearerEvents(IJwtService jwtService)
+        {
+            this.jwtService = jwtService;
+        }
+
+        public override async Task TokenValidated(TokenValidatedContext context)
+        {
+            string? bearer = context.Request.Headers.Authorization.FirstOrDefault();
+            string token = this.jwtService.RemoveBearer(bearer);
+
+            bool isBlacklisted = await this.jwtService.CheckIfJWTHasBeenInvalidated(token);
+            if (isBlacklisted)
+            {
+               context.Fail("BLACKLISTED TOKEN");
+            }
+            await base.TokenValidated(context);
+        }
+    }
+}
