565e025f72ba047509f25a31946ca0728852c434
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-08 14:22:41 +0300

    add a GuestsOnly filter

---
+++ QuizWorld.Infrastructure/Filters/GuestsOnly/GuestsOnlyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Infrastructure.Filters.GuestsOnly
+{
+    /// <summary>
+    /// Prevents access to a given route or controller by logged in users. Only guests can
+    /// access it.
+    /// </summary>
+    public class GuestsOnlyAttribute : ServiceFilterAttribute
+    {
+        public GuestsOnlyAttribute() : base(typeof(GuestsOnlyFilter))
+        {
+        }
+    }
+}
---
+++ QuizWorld.Infrastructure/Filters/GuestsOnly/GuestsOnlyFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using QuizWorld.Infrastructure.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Infrastructure.Filters.GuestsOnly
+{
+    /// <summary>
+    /// Prevents access to a given route or controller by logged in users. Only guests can
+    /// access it.
+    /// </summary>
+    public class GuestsOnlyFilter : IAsyncAuthorizationFilter
+    {
+        private readonly IJwtService jwtService;
+        private readonly IHttpContextAccessor http;
+        public GuestsOnlyFilter(IJwtService jwtService, IHttpContextAccessor http)
+        {
+            this.jwtService = jwtService;
+            this.http = http;
+        }
+
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            string? bearer = http.HttpContext?.Request.Headers.Authorization.FirstOrDefault();
+            string token = jwtService.RemoveBearer(bearer);
+
+            bool isValid = await jwtService.CheckIfJWTIsValid(token);
+            if (isValid)
+            {
+                context.Result = new ForbidResult("Bearer");
+                return;
+            }
+        }
+    }
+}
