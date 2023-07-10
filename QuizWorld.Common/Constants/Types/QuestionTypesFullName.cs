f48e0605455532e08e047b5cc957c2099ff30145
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-11 01:31:12 +0300

    add string constants for question types

---
+++ QuizWorld.Common/Constants/Types/QuestionTypesFullName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Common.Constants.Types
+{
+    /// <summary>
+    /// Full names for each question type.
+    /// </summary>
+    public static class QuestionTypesFullNames
+    {
+        public const string SingleChoice = "Single-choice";
+        public const string MultipleChoice = "Multiple-choice";
+        public const string Text = "Text";
+    }
+}
