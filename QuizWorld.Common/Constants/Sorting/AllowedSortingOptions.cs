6a1f2e13cf89203e45cbcc852a6c1eae30128844
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-13 01:51:33 +0300

    add some constants and enums for sorting options

---
+++ QuizWorld.Common/Constants/Sorting/AllowedSortingOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Common.Constants.Sorting
+{
+    public static class AllowedSortingOptions
+    {
+        public static readonly Dictionary<string, SortingCategories> Categories = new()
+        {
+            { "title", SortingCategories.Title },
+            { "createdOn", SortingCategories.CreatedOn },
+            { "updatedOn", SortingCategories.UpdatedOn },
+        };
+
+        public static readonly Dictionary<string, SortingOrders> Orders = new()
+        {
+            { "asc", SortingOrders.Ascending },
+            { "desc", SortingOrders.Descending },
+        };
+    }
+}
