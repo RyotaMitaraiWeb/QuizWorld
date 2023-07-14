6daec7ec82cc8ee7719f0ba09edc210c51ccce4e
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-15 00:27:24 +0300

    fix build error

--- QuizWorld.ViewModels/Question/CreateQuestionViewModel.cs
+++ QuizWorld.ViewModels/Question/CreateQuestionViewModel.cs
@@ -4,31 +4,28 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuizWorld.Common.Constants.Types;
 using QuizWorld.Common.Constants.ValidationErrorMessages;
 using QuizWorld.Common.Constants.ValidationRules;
+using QuizWorld.ViewModels.Answer;
 using QuizWorld.ViewModels.Question;
+using QuizWorld.ViewModels.Validators;
 
 namespace QuizWorld.ViewModels.Quiz
 {
     /// <summary>
     /// Represents a quiz that is being created.
     /// </summary>
-    public class CreateQuizViewModel
+    public class CreateQuestionViewModel
     {
-        [Required(ErrorMessage = QuizValidationErrorMessages.Title.IsEmpty)]
-        [MinLength(QuizValidationRules.Title.MinLength, ErrorMessage = QuizValidationErrorMessages.Title.IsTooShort)]
-        [MaxLength(QuizValidationRules.Title.MaxLength, ErrorMessage = QuizValidationErrorMessages.Title.IsTooLong)]
-        public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = QuestionValidationErrorMessages.Prompt.IsEmpty)]
+        [MaxLength(QuestionValidationRules.Prompt.MaxLength, ErrorMessage = QuestionValidationErrorMessages.Prompt.IsEmpty)]
+        public string Prompt { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = QuizValidationErrorMessages.Description.IsEmpty)]
-        [MinLength(QuizValidationRules.Title.MinLength, ErrorMessage = QuizValidationErrorMessages.Description.IsTooShort)]
-        [MaxLength(QuizValidationRules.Title.MaxLength, ErrorMessage = QuizValidationErrorMessages.Description.IsTooLong)]
-        public string Description { get; set; } = string.Empty;
+        public QuestionTypes Type { get; set; }
 
-        public bool InstantMode { get; set; } = false;
-
-        [MinLength(QuizValidationRules.Questions.MininmumAmount, ErrorMessage = QuizValidationErrorMessages.Questions.NotEnoughQuestions)]
-        [MaxLength(QuizValidationRules.Questions.MaximumAmount, ErrorMessage = QuizValidationErrorMessages.Questions.TooManyQuestions)]
-        public IEnumerable<CreateQuestionViewModel> Questions { get; set; } = Enumerable.Empty<CreateQuestionViewModel>();
+        [HasEnoughAnswers]
+        [CorrectAndWrongAnswersAmount]
+        public IEnumerable<CreateAnswerViewModel> Answers { get; set; } = Enumerable.Empty<CreateAnswerViewModel>();
     }
 }
