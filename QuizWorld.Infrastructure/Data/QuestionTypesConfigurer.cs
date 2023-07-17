b3c333cb085b392a83a3f7667a87cc49ab6f0796
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-18 01:05:39 +0300

    add migration and question types seeding

---
+++ QuizWorld.Infrastructure/Data/QuestionTypesConfigurer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuizWorld.Common.Constants.Types;
+using QuizWorld.Infrastructure.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Infrastructure.Data
+{
+    public class QuestionTypesConfigurer : IEntityTypeConfiguration<QuestionType>
+    {
+        public void Configure(EntityTypeBuilder<QuestionType> builder)
+        {
+            builder.HasData(
+                new QuestionType()
+                {
+                    Id = 1,
+                    Type = QuestionTypes.SingleChoice,
+                    FullName = "Single-choice",
+                    ShortName = "single",
+                },
+                new QuestionType()
+                {
+                    Id = 2,
+                    Type = QuestionTypes.MultipleChoice,
+                    FullName = "Multiple-choice",
+                    ShortName = "multi",
+                },
+                new QuestionType()
+                {
+                    Id = 3,
+                    Type = QuestionTypes.Text,
+                    FullName = "Text",
+                    ShortName = "text",
+                }
+            );
+        }
+    }
+}
