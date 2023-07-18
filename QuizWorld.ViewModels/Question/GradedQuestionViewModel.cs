0c43d2fea23883b9755b5e589d72d51f1cd645f0
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-18 13:26:23 +0300

    add GradedQuestionViewModel

---
+++ QuizWorld.ViewModels/Question/GradedQuestionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using QuizWorld.ViewModels.Answer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.ViewModels.Question
+{
+    /// <summary>
+    /// Represents a question with all of its correct answers.
+    /// </summary>
+    public class GradedQuestionViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public IEnumerable<AnswerViewModel> Answers = Enumerable.Empty<AnswerViewModel>();
+    }
+}
