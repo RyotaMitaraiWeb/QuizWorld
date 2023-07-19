0a21e3dfed0717b0666b6af9802dfee0c85c2878
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-20 00:55:33 +0300

    add ListUserViewModel

---
+++ QuizWorld.ViewModels/UserList/ListUserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.ViewModels.UserList
+{
+    /// <summary>
+    /// Represents a user in a list of users.
+    /// </summary>
+    public class ListUserViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        /// <summary>
+        /// A collection of roles joined by a comma and space (", "). The role "User" is
+        /// removed from the list if the user has any other roles.
+        /// </summary>
+        public string Roles { get; set; } = string.Empty;
+    }
+}
