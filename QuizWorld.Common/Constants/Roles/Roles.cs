3737cedd930231bd5376ce00b7b9e6b2f260dec3
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-20 01:13:59 +0300

    add some role constants

---
+++ QuizWorld.Common/Constants/Roles/Roles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Common.Constants.Roles
+{
+    /// <summary>
+    /// Constants for roles
+    /// </summary>
+    public static class Roles
+    {
+        /// <summary>
+        /// Administrators can check activity logs and can change users' roles to a limited extent.
+        /// </summary>
+        public const string Admin = "Administrator";
+
+        /// <summary>
+        /// Moderators can delete and edit other users' quizzes.
+        /// </summary>
+        public const string Moderator = "Moderator";
+
+        /// <summary>
+        /// This is the default role of each registered user.
+        /// </summary>
+        public const string User = "User";
+
+        /// <summary>
+        /// Contains all roles that can be given to or removed from any user. Roles not present
+        /// here can only be managed with seeding or at database level.
+        /// </summary>
+        public readonly static string[] RolesThatCanBeGivenOrRemoved = new[]
+        {
+            Moderator, User 
+        };
+    }
+}
