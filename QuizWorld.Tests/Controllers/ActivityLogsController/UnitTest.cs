7c4e7b77fe511fe0dba5a1404b33f55dfc735663
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-19 17:34:07 +0300

    prepare controller and unit tests

---
+++ QuizWorld.Tests/Controllers/ActivityLogsController/UnitTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MockQueryable.Moq;
+using Moq;
+using QuizWorld.Common.Constants.Sorting;
+using QuizWorld.ViewModels.Logging;
+using QuizWorld.Web.Areas.Logging.Controllers;
+using QuizWorld.Web.Contracts.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Tests.Controllers.ActivityLogsControllerUnitTests
+{
+    public class UnitTest
+    {
+        public Mock<IActivityLogger> loggerMock;
+        public ActivityLogsController controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.loggerMock = new Mock<IActivityLogger>();
+            this.controller = new ActivityLogsController(this.loggerMock.Object);
+        }
+
+        [Test]
+        public async Task Test_GetLogsReturnsAListOfLogs()
+        {
+            var date = DateTime.Now;
+            var list = new List<ActivityLogViewModel>()
+            {
+                new ActivityLogViewModel
+                {
+                    Id = "a",
+                    Message = "test",
+                    Date = date
+                }
+            };
+
+            var mock = list.BuildMock();
+
+            this.loggerMock
+                .Setup(l => l.RetrieveLogs(1, SortingOrders.Ascending, 6))
+                .ReturnsAsync(mock);
+
+            var result = await this.controller.GetLogs(1, SortingOrders.Ascending) as OkObjectResult;
+            var value = result.Value as List<ActivityLogViewModel>;
+
+            Assert.That(value.First().Message, Is.EqualTo("test"));
+        }
+
+        [Test]
+        public async Task Test_GetLogsReturnsNotFoundIfLoggerThrows()
+        {
+            this.loggerMock
+                .Setup(l => l.RetrieveLogs(1, SortingOrders.Ascending, 6))
+                .ThrowsAsync(new Exception());
+
+            var result = await this.controller.GetLogs(1, SortingOrders.Ascending);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+    }
+}
---
+++ QuizWorld.Web/Areas/Logging/Controllers/ActivityLogsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QuizWorld.Common.Constants.Sorting;
+using QuizWorld.Infrastructure.ModelBinders;
+using QuizWorld.Web.Contracts.Logging;
+
+namespace QuizWorld.Web.Areas.Logging.Controllers
+{
+    [ApiController]
+    [Route("/logs")]
+    public class ActivityLogsController : BaseController
+    {
+        private readonly IActivityLogger logger;
+        public ActivityLogsController(IActivityLogger logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "CanAccessLogs", AuthenticationSchemes = "Bearer")]
+        public Task<ActionResult> GetLogs(
+            [ModelBinder(BinderType = typeof(PaginationModelBinder))] int page,
+            [ModelBinder(BinderType = typeof(SortingOrderModelBinder))] SortingOrders order
+            )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
