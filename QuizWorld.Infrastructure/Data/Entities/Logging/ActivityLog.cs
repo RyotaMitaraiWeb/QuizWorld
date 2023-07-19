4372b0269d0ddaa3eb5bca083cd3a2ca6c2aeb8d
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-19 12:49:47 +0300

    add ActivityLog entity

---
+++ QuizWorld.Infrastructure/Data/Entities/Logging/ActivityLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWorld.Infrastructure.Data.Entities.Logging
+{
+    public class ActivityLog
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; } = null!;
+        public DateTime Date { get; set; }
+    }
+}
