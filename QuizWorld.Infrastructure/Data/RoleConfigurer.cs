6641c95f337ef18b29c171e5087962fa5c37f946
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-21 23:26:09 +0300

    add IDs to role seeds and add an entire new migration

--- QuizWorld.Infrastructure/Data/RoleConfigurer.cs
+++ QuizWorld.Infrastructure/Data/RoleConfigurer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuizWorld.Infrastructure.Data.Entities.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,27 +10,27 @@ using System.Threading.Tasks;
 
 namespace QuizWorld.Infrastructure.Data
 {
-    public class RoleConfigurer : IEntityTypeConfiguration<IdentityRole<Guid>>
+    public class RoleConfigurer : IEntityTypeConfiguration<ApplicationRole>
     {
-        public void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder)
+        public void Configure(EntityTypeBuilder<ApplicationRole> builder)
         {
             builder.HasData
             (
-                new IdentityRole<Guid>()
+                new ApplicationRole()
                 {
-                    Id = Guid.NewGuid(),
+                    Id = new Guid("CACE07A2-930C-4029-9465-019D78EDCF68"),
                     Name = "User",
                     NormalizedName = "USER",
                 },
-                new IdentityRole<Guid>()
+                new ApplicationRole()
                 {
-                    Id = Guid.NewGuid(),
+                    Id = new Guid("90F00FD4-9966-4819-AA58-98836F0E0DDF"),
                     Name = "Administrator",
                     NormalizedName = "ADMINISTRATOR",
                 },
-                new IdentityRole<Guid>()
+                new ApplicationRole()
                 {
-                    Id = Guid.NewGuid(),
+                    Id = new Guid("6448A395-D249-4AEF-BF74-9CDEEBA69F33"),
                     Name = "Moderator",
                     NormalizedName = "MODERATOR",
                 }
