8b331bdd198956a4d74cef1e1b0bd20d5f4a5e24
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-05-27 13:51:38 +0300

    refactor create endpoint

--- QuizWorld.Web/Controllers/QuizController.cs
+++ QuizWorld.Web/Controllers/QuizController.cs
@@ -4,15 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 using QuizWorld.Common.Errors;
 using QuizWorld.Common.Http;
 using QuizWorld.Common.Search;
+using QuizWorld.ViewModels.Authentication;
+using QuizWorld.ViewModels.Common;
+using QuizWorld.ViewModels.Quiz;
+using QuizWorld.Web.Contracts.Authentication.JsonWebToken;
 using QuizWorld.Web.Contracts.Quiz;
 
 namespace QuizWorld.Web.Controllers
 {
     [Route("quiz")]
     [ApiVersion("2.0")]
-    public class QuizController(IQuizService quizService) : BaseController
+    public class QuizController(IQuizService quizService, IJwtService jwtService) : BaseController
     {
         private readonly IQuizService _quizService = quizService;
+        private readonly IJwtService _jwtService = jwtService;
 
         [Route("")]
         [HttpGet]
@@ -37,5 +42,47 @@ namespace QuizWorld.Web.Controllers
 
             return Ok(result.Value);
         }
+
+        [Route("")]
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            CreateQuizViewModel quiz,
+            [FromHeader(Name = "Authorization")] string? bearerToken
+            )
+        {
+            CreateOrEditQuizMetadata metadata = await ExtractQuizMetadata(bearerToken);
+
+            int createdQuizId = await _quizService.CreateAsync(
+                quiz: quiz,
+                userId: metadata.User.Id,
+                creationDate: metadata.Now);
+
+            CreatedResponseViewModel response = new()
+            {
+                Id = createdQuizId
+            };
+
+            return Created($"/quiz/{createdQuizId}", response);
+
+        }
+
+        private async Task<CreateOrEditQuizMetadata> ExtractQuizMetadata(string? bearerToken)
+        {
+            var claimsResult = await _jwtService.ExtractUserFromTokenAsync(bearerToken ?? string.Empty);
+            UserViewModel user = claimsResult.Value;
+            var now = DateTime.Now;
+
+            return new CreateOrEditQuizMetadata()
+            {
+                Now = now,
+                User = user,
+            };
+        }
+    }
+
+    internal class CreateOrEditQuizMetadata
+    {
+        public DateTime Now { get; set; }
+        public UserViewModel User { get; set; } = null!;
     }
 }
