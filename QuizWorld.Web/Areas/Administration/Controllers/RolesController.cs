49a534915bb92f68d7d9416800d191be6e6142aa
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-08 16:42:45 +0300

    refactor endpoints

--- QuizWorld.Web/Areas/Administration/Controllers/RolesController.cs
+++ QuizWorld.Web/Areas/Administration/Controllers/RolesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using QuizWorld.Common.Search;
 using QuizWorld.ViewModels.Roles;
 using QuizWorld.Web.Contracts.Roles;
+using static QuizWorld.Common.Errors.RoleError;
 
 namespace QuizWorld.Web.Areas.Administration.Controllers
 {
@@ -12,12 +13,51 @@ namespace QuizWorld.Web.Areas.Administration.Controllers
     {
         private readonly IRoleService _roleService = roleService;
 
-        [HttpGet]
-        [Route("users")]
+        [HttpGet("users")]
         public async Task<IActionResult> SearchUsers([FromQuery] SearchUsersParameters parameters)
         {
             var result = await _roleService.SearchUsers(parameters);
             return Ok(result);
+        }
+
+        [HttpPatch("add")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> AddRoleToUser(ChangeRoleViewModel model)
+        {
+            var result = await _roleService.GiveUserRole(model);
+            if (result.IsFailure)
+            {
+                if (result.Error == AddRoleError.UserDoesNotExist)
+                {
+                    return NotFound();
+                }
+
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPatch("remove")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> RemoveRoleFromUser(ChangeRoleViewModel model)
+        {
+            var result = await _roleService.RemoveRoleFromUser(model);
+            if (result.IsFailure)
+            {
+                if (result.Error == RemoveRoleError.UserDoesNotExist)
+                {
+                    return NotFound();
+                }
+
+                return BadRequest();
+            }
+
+            return NoContent();
         }
     }
 }
---
+++ Tests.Unit/Controllers/RolesControllerTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using QuizWorld.Common.Constants.Roles;
+using QuizWorld.Common.Result;
+using QuizWorld.ViewModels.Roles;
+using QuizWorld.Web.Areas.Administration.Controllers;
+using QuizWorld.Web.Contracts.Roles;
+using static QuizWorld.Common.Errors.RoleError;
+
+namespace Tests.Unit.Controllers
+{
+    public class RolesControllerTests
+    {
+        public IRoleService RoleService { get; set; }
+        public RolesController RolesController { get; set; }
+        public ChangeRoleViewModel ChangeRoleViewModel { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            RoleService = Substitute.For<IRoleService>();
+            RolesController = new RolesController(RoleService);
+            ChangeRoleViewModel = new()
+            {
+                UserId = Guid.NewGuid().ToString(),
+                Role = Roles.Moderator,
+            };
+        }
+
+        [Test]
+        public async Task Test_AddRoleToUserReturnsNoContentWhenSuccessful()
+        {
+            var mockResult = Result<string, AddRoleError>.Success("a");
+
+            RoleService
+                .GiveUserRole(ChangeRoleViewModel)
+                .Returns(mockResult);
+
+            var result = await RolesController.AddRoleToUser(ChangeRoleViewModel);
+            Assert.That(result, Is.TypeOf<NoContentResult>());
+        }
+
+        [Test]
+        [TestCase(AddRoleError.UserDoesNotExist, 404)]
+        [TestCase(AddRoleError.UserAlreadyHasRole, 400)]
+        public async Task Test_AddRoleToUserReturnsCorrectStatusCodeWhenAnErrorIsReturnedFromService(AddRoleError error, int statusCode)
+        {
+            var mockResult = Result<string, AddRoleError>.Failure(error);
+
+            RoleService
+                .GiveUserRole(ChangeRoleViewModel)
+                .Returns(mockResult);
+
+            var result = await RolesController.AddRoleToUser(ChangeRoleViewModel);
+            var response = result as StatusCodeResult;
+
+            Assert.That(response?.StatusCode, Is.EqualTo(statusCode));
+        }
+
+        [Test]
+        public async Task Test_RemoveRoleFromUserReturnsNoContentWhenSuccessful()
+        {
+            var mockResult = Result<string, RemoveRoleError>.Success("a");
+
+            RoleService
+                .RemoveRoleFromUser(ChangeRoleViewModel)
+                .Returns(mockResult);
+
+            var result = await RolesController.RemoveRoleFromUser(ChangeRoleViewModel);
+            Assert.That(result, Is.TypeOf<NoContentResult>());
+        }
+
+        [Test]
+        [TestCase(RemoveRoleError.UserDoesNotExist, 404)]
+        [TestCase(RemoveRoleError.UserDoesNotHaveRoleInFirstPlace, 400)]
+        public async Task Test_RemoveRoleFromUserReturnsCorrectStatusCodeWhenAnErrorIsReturnedFromService(RemoveRoleError error, int statusCode)
+        {
+            var mockResult = Result<string, RemoveRoleError>.Failure(error);
+
+            RoleService
+                .RemoveRoleFromUser(ChangeRoleViewModel)
+                .Returns(mockResult);
+
+            var result = await RolesController.RemoveRoleFromUser(ChangeRoleViewModel);
+            var response = result as StatusCodeResult;
+
+            Assert.That(response?.StatusCode, Is.EqualTo(statusCode));
+        }
+    }
+}
