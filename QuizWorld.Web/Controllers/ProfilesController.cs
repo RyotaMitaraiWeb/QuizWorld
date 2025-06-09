336af7e85deecd7d1eb52d80dd4269664cd4e04a
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-09 19:19:19 +0300

    implement controller

---
+++ QuizWorld.Web/Controllers/ProfilesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QuizWorld.Common.Search;
+using QuizWorld.Web.Contracts;
+
+namespace QuizWorld.Web.Controllers
+{
+    [Route("profile")]
+    public class ProfilesController(IProfileService profileService) : BaseController
+    {
+        private readonly IProfileService _profileService = profileService;
+
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> SearchProfiles([FromQuery] SearchUsersParameters parameters)
+        {
+            var result = await _profileService.SearchUsers(parameters);
+            return Ok(result);
+        }
+
+        [HttpGet("username/{username}")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetProfileByUsername(string username)
+        {
+            var result = await _profileService.GetUserByUsername(username);
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Tests.Unit/Controllers/ProfilesControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using QuizWorld.ViewModels.Authentication;
+using QuizWorld.Web.Contracts;
+using QuizWorld.Web.Controllers;
+
+namespace Tests.Unit.Controllers
+{
+    public class ProfilesControllerTests
+    {
+        public IProfileService ProfileService { get; set; }
+        public ProfilesController ProfilesController { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            ProfileService = Substitute.For<IProfileService>();
+            ProfilesController = new ProfilesController(ProfileService);
+        }
+
+        [Test]
+        public async Task Test_GetProfileByUsernameReturnsNotFoundIfAProfileCannotBeFound()
+        {
+            UserViewModel? user = null;
+            string username = "a";
+
+            ProfileService
+                .GetUserByUsername(username)
+                .Returns(user);
+
+            var result = await ProfilesController.GetProfileByUsername(username);
+
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task Test_GetProfileByUsernameReturnsOKfAProfileIsFound()
+        {
+            UserViewModel? user = new UserViewModel();
+            string username = "a";
+
+            ProfileService
+                .GetUserByUsername(username)
+                .Returns(user);
+
+            var result = await ProfilesController.GetProfileByUsername(username);
+
+            Assert.That(result, Is.TypeOf<OkObjectResult>());
+        }
+    }
+}
