7ef205dd83694019811fb7cf483634eee8d575eb
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-23 03:19:10 +0300

    configure SignalR

---
+++ QuizWorld.Common/Hubs/HubEndpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizWorld.Common.Hubs
+{
+    public static class HubEndpoints
+    {
+        public const string HubsPrefix = "/hubs";
+        public const string Session = "/hubs/session";
+    }
+}
