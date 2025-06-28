9736cb72370355027a0e41d129aa6442b354beac
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-29 02:51:49 +0300

    Reapply "create validator for file length"
    This reverts commit 1b3d90a6402b4c630d82b5c934fd740e9a01f954.

---
+++ QuizWorld.Common/Bytes/ByteUnits.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizWorld.Common.Bytes
+{
+    /// <summary>
+    /// All enum values are mapped to their powers of 1024 
+    /// (for example, megabytes are 1024 bytes to the power of 2), which you can cast to
+    /// integers if required.
+    /// </summary>
+    public enum ByteUnits
+    {
+        Bytes = 0,
+        Kilobytes = 1,
+        Megabytes = 2,
+    }
+}
