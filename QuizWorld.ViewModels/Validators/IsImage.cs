207e255b1dfcadda4738ced273f4748e7d0ad33e
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-29 02:52:55 +0300

    add IsImage validator

---
+++ QuizWorld.ViewModels/Validators/IsImage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuizWorld.ViewModels.Validators
+{
+    public class IsImage : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not IFormFile file)
+                return new ValidationResult(NotAFileErrorMessage);
+
+            try
+            {
+                using var stream = file.OpenReadStream();
+                var imageInfo = SixLabors.ImageSharp.Image.Identify(stream);
+
+                if (imageInfo is null)
+                    return new ValidationResult(NotAnImageErrorMessage);
+
+                return ValidationResult.Success;
+            }
+            catch
+            {
+                return new ValidationResult(NotAnImageErrorMessage);
+            }
+        }
+
+        private static readonly string NotAFileErrorMessage = "The provided value is not a file";
+        private static readonly string NotAnImageErrorMessage = "The file is not a valid image.";
+    }
+}
