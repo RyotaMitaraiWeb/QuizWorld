b12612b306347d4dbedd49e41376101cfeff0b5a
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-29 17:46:04 +0300

    forgot to commit view models

---
+++ QuizWorld.ViewModels/Image/ImageDimensionOptionsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizWorld.ViewModels.Image
+{
+    public class ImageDimensionOptionsViewModel
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
---
+++ QuizWorld.ViewModels/Profile/UploadProfilePictureViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using QuizWorld.Common.Constants.ValidationRules;
+using QuizWorld.ViewModels.Validators;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuizWorld.ViewModels.Profile
+{
+    public class UploadProfilePictureViewModel
+    {
+        [Required]
+        [MaxFileLength(ProfileValidationConstants.ProfilePictureMaxLengthInMegaBytes)]
+        [IsImage]
+        public IFormFile File { get; set; } = default!;
+    }
+}
